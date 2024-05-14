d8d7dbe11f86c48137f8e66f9eee25b5f55f77ab
Jaxon Bryant <jbryant18@piopio.school.nz>
2024-05-15 11:07:07 +1200

    Seperated HTML and CSS
    Also added image folder

--- index.php
+++ index.php
@@ -4,125 +4,9 @@
 <title>Bryant Contracting: Home</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/style.css">
 <style>
-body {
-    font-family: Arial, Helvetica, sans-serif;
-}
-    
-.header {
-    padding: 10px; /* Some padding */
-    background: #ffffff; /* White background */
-    color: #0F5010; /* Green text color */
-    display: flex; /* Added */
-    justify-content: space-between; /* Added */
-    align-items: center; /* Moves content in the centre */
-}
 
-.logo {
-    flex-shrink: 0; /* Courtesy of ChatGPT */
-    margin-right: 20px; 
-}
-
-.text {
-    text-align: right; /* Moves the text to the right */
-}
-
-/* Increase the font size of the <h1> element */
-.header h1 {
-    font-size: 40px;
-}
-    
-/* Style the top navigation bar */
-.navbar {
-    overflow: hidden; /* Hide overflow */
-    background-color: #0F5010; /* green background color */
-}
-
-/* Style the navigation bar links */
-.navbar a {
-    float: left; /* Make sure that the links stay side-by-side */
-    display: block; /* Change the display to block, for responsive reasons */
-    color: white; /* White text color */
-    text-align: center; /* Center the text */
-    padding: 14px 20px; /* Some padding */
-    text-decoration: none; /* Remove underline */
-}
-
-/* Right-aligned link */
-.navbar a.right {
-    float: right; /* Float a link to the right */
-}
-
-/* Change color on hover/mouse-over */
-.navbar a:hover {
-    background-color: #ddd; /* Grey background color */     /* NTS: Change this color */
-    color: black; /* Black text color */
-}
-    
-/* Ensure proper sizing */
-* {
-    box-sizing: border-box;
-}
-
-/* Column container */
-.row {
-    display: flex;
-    flex-wrap: wrap;
-}
-
-/* Create two unequal columns that sits next to each other */    /* This may be changed */
-/* Sidebar/left column */
-.side {
-    flex: 30%; /* Set the width of the sidebar */
-    background-color: #f1f1f1; /* Grey background color */
-    padding: 20px; /* Some padding */
-}
-
-/* Main column */
-.main {
-    flex: 70%; /* Set the width of the main content */
-    background-color: white; /* White background color */
-    padding: 20px; /* Some padding */
-}
-
-/* Responsive layout - when the screen is less than 700px wide, make the two columns stack on top of each other instead of next to each other */
-@media screen and (max-width: 700px) {
-    .row {
-    flex-direction: column;
-    }
-}
-
-/* Responsive layout - when the screen is less than 400px wide, make the navigation links stack on top of each other instead of next to each other */
-@media screen and (max-width: 400px) {
-  .navbar a {
-    float: none;
-    width: 100%;
-    }
-}
-    
-/* Media query for mobile screens */
-@media screen and (max-width: 768px) {
-    .header {
-        flex-direction: column; /* Stack items vertically */
-        align-items: center; /* Align items to center */
-    }
-    .text {
-        text-align: center; /* Align text to center */
-        margin-top: 10px; /* Add some space between image and text */
-    }
-    .logo {
-        margin-right: 0; /* Remove right margin to align image with text */
-        margin-left: 31px; /* Manually move the logo to the right to visually align with text */
-        margin-bottom: 10px; /* Add margin to center vertically */
-    }
-}
-    
-.footer {
-    padding: 20px; /* Some padding */
-    text-align: left; /* Left text*/
-    background: #0F5010; /* Green background */
-    color: #ffffff; /* White text color */
-}
 </style>
 </head>
 <body>
@@ -130,7 +14,7 @@ body {
 
 <div class="header">
     <div class="logo">
-        <img src="https://live.staticflickr.com/65535/53710731185_fe74e26743_o.jpg" alt="Bryant Contracting" style="width: 90%;">
+        <img src="images/bryant_contracting_logo.jpg" alt="Bryant Contracting" style="width: 90%;">
     </div>
     <div class="text">
         <h1>027 215 4213</h1>

a687f636d14712110ec29b29450924458bfe1c92
Jaxon Bryant <jbryant18@piopio.school.nz>
2024-05-30 14:15:33 +1200

    Broken version
    The styling doesn't work after I changed some of the tags names, and I tryed to use chatGPT to try to fix it, so I don't understand it entirely.

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<div class="header">
+<header>
     <div class="logo">
         <img src="images/bryant_contracting_logo.jpg" alt="Bryant Contracting" style="width: 90%;">
     </div>
@@ -6,13 +6,13 @@
         <h1>027 215 4213</h1>
         <h3>nik@bryantcontracting.co.nz</h3>
     </div>
-</div>
+</header>
 
-<div class="navbar">
+<nav>
     <a href="#">Home</a>
     <a href="#">Agriculture</a>
     <a href="#">Earthworks</a>
     <a href="#">Cartage</a>
     <a href="#">Gallery</a>
     <a href="#" class="right">Contact</a>
-</div>
+</nav>
--- index.php
+++ index.php
@@ -5,32 +5,33 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/style.css">
+    
 <style>
-
 </style>
+    
 </head>
 <body>
 <?php include 'header.php';?>
     
 
-    <div class="section">
+    <section>
         <img src="Images/bryant_contracting_bgimage_1.JPG" alt="Image 1">
         <div class="content">
             <div class="text-box">Text for Image 1</div>
         </div>
-    </div>
-    <div class="section">
+    </section>
+    <section>
         <img src="Images/bryant_contracting_bgimage_1.JPG" alt="Image 2">
         <div class="content">
             <div class="text-box">Text for Image 2</div>
         </div>
-    </div>
-    <div class="section">
+    </section>
+    <section>
         <img src="Images/bryant_contracting_bgimage_1.JPG" alt="Image 3">
         <div class="content">
             <div class="text-box">Text for Image 3</div>
         </div>
-    </div>
+    </section>
 
 <?php include 'footer.php';?>
     

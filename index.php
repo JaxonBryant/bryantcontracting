6c6a1baa57763617ac349f88e6575e9e310c8f42
Jaxon Bryant <jbryant18@piopio.school.nz>
2024-06-05 11:12:22 +1200

    Broken 2

--- index.php
+++ index.php
@@ -12,26 +12,29 @@
 </head>
 <body>
 <?php include 'header.php';?>
-    
 
-    <section>
-        <img src="Images/bryant_contracting_bgimage_1.JPG" alt="Image 1">
-        <div class="content">
-            <div class="text-box">Text for Image 1</div>
-        </div>
-    </section>
-    <section>
-        <img src="Images/bryant_contracting_bgimage_1.JPG" alt="Image 2">
-        <div class="content">
-            <div class="text-box">Text for Image 2</div>
-        </div>
-    </section>
-    <section>
-        <img src="Images/bryant_contracting_bgimage_1.JPG" alt="Image 3">
-        <div class="content">
-            <div class="text-box">Text for Image 3</div>
-        </div>
-    </section>
+    <div class="container">
+        <section>
+            <img src="Images/bryant_contracting_bgimage_1.JPG" alt="Image 1">
+            <div class="content">
+                <div class="text-box">Text for Image 1</div>
+            </div>
+        </section>
+
+        <section>
+            <img src="Images/bryant_contracting_bgimage_1.JPG" alt="Image 2">
+            <div class="content">
+                <div class="text-box">Text for Image 2</div>
+            </div>
+        </section>
+
+        <section>
+            <img src="Images/bryant_contracting_bgimage_1.JPG" alt="Image 3">
+            <div class="content">
+                <div class="text-box">Text for Image 3</div>
+            </div>
+        </section>
+    </div>
 
 <?php include 'footer.php';?>
     

1a09518b11d8fd30009f0de8a4be0768e7513c09
Jaxon Bryant <jbryant18@piopio.school.nz>
2024-06-05 11:19:41 +1200

    Old version reloaded

--- index.php
+++ index.php
@@ -5,35 +5,31 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/style.css">
-    
 <style>
+
 </style>
-    
 </head>
 <body>
 <?php include 'header.php';?>
+    
 
-    <div class="container">
-        <section>
-            <img src="Images/bryant_contracting_bgimage_1.JPG" alt="Image 1">
-            <div class="content">
-                <div class="text-box">Text for Image 1</div>
-            </div>
-        </section>
-
-        <section>
-            <img src="Images/bryant_contracting_bgimage_1.JPG" alt="Image 2">
-            <div class="content">
-                <div class="text-box">Text for Image 2</div>
-            </div>
-        </section>
-
-        <section>
-            <img src="Images/bryant_contracting_bgimage_1.JPG" alt="Image 3">
-            <div class="content">
-                <div class="text-box">Text for Image 3</div>
-            </div>
-        </section>
+    <div class="section">
+        <img src="Images/bryant_contracting_bgimage_1.JPG" alt="Image 1">
+        <div class="content">
+            <div class="text-box">Text for Image 1</div>
+        </div>
+    </div>
+    <div class="section">
+        <img src="Images/bryant_contracting_bgimage_1.JPG" alt="Image 2">
+        <div class="content">
+            <div class="text-box">Text for Image 2</div>
+        </div>
+    </div>
+    <div class="section">
+        <img src="Images/bryant_contracting_bgimage_1.JPG" alt="Image 3">
+        <div class="content">
+            <div class="text-box">Text for Image 3</div>
+        </div>
     </div>
 
 <?php include 'footer.php';?>

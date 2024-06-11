2779da837d5550cecaa6c6a3fc8ec51449e38bfe
Jaxon Bryant <jbryant18@piopio.school.nz>
2024-06-11 14:17:22 +1200

    Fixed (working home page)
    Added fonts (untested)
    Made home page responsive

--- index.php
+++ index.php
@@ -1,34 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Add fonts across the whole website -->
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="css/style.css">
+    <!-- page setup -->
     <title>Bryant Contracting: Home</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/style.css">
-<style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/style.css">
 
+<style>
 </style>
+
 </head>
+
 <body>
 <?php include 'header.php';?>
     
 
     <div class="section">
         <img src="Images/bryant_contracting_bgimage_1.JPG" alt="Image 1">
-        <div class="content">
-            <div class="text-box">Text for Image 1</div>
+        <div class="content-right">
+            <h2 class="large-text">About us</h2>
+            <p class="medium-text">
+                BRYANT CONTRACTING LTD was established in September 2006. Managing Director Nik Bryant is the driving force behind the business and is both an experienced machine operator as well as a respected operations manager. If there is a job on your farm that needs doing we have the gear and the knowledge to get the job done.
+            </p>
+
         </div>
     </div>
+    
     <div class="section">
         <img src="Images/bryant_contracting_bgimage_1.JPG" alt="Image 2">
-        <div class="content">
-            <div class="text-box">Text for Image 2</div>
+        <div class="content-left">
+            <h2 class="large-text">Our services</h2>
+            <p class="medium-text">
+                Bryant Contracting are able to do a large range of earthworks and agricultural work—including farm drainage, contouring, races, spraying, seeding, hay/silage and fertilizer cartage and spreading.
+                We also provide cartage services.
+            </p>
         </div>
     </div>
+    
     <div class="section">
         <img src="Images/bryant_contracting_bgimage_1.JPG" alt="Image 3">
-        <div class="content">
-            <div class="text-box">Text for Image 3</div>
+        <div class="content-right">
+            <h2 class="large-text">Our equipment</h2>
+            <p class="medium-text">
+                Our up to date machines include Excavators, bulldozers, truck/trailer/transporter, grader, tractors with both trailers and a full compliment of agricultural attachments—boom sprayer, discs, rotor-tiller, power harrow, roller drill, direct drill, mower, rake, baler/wrapper and more.
+            </p>
         </div>
     </div>
 

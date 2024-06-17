5370e04b02bb3765cecf25c8e11c177548ed2f0a
Jaxon Bryant <jbryant18@piopio.school.nz>
2024-06-18 11:01:39 +1200

    Improved Gallery 2
    Entirely changed the code and the layout to something with better futureproofing and aesthetics

--- gallery.php
+++ gallery.php
@@ -20,59 +20,47 @@
 <body>
 <?php include 'header.php';?>
     
-<h2>Gallery</h2>
-
-<div class="responsive">
-    <div class="gallery">
-        <a target="_blank" href="Images/bryant_contracting_image_1.JPG">
-            <img src="Images/bryant_contracting_image_1.JPG" alt="Image 1" width="600" height="400">
-        </a>
-    </div>
+<br>
+    
+<div class="row">
+  <div class="column">
+      <img src="Images/bryant_contracting_bgimage_1.JPG" class="gallery">
+      <img src="Images/bryant_contracting_image_1.JPG" class="gallery">
+      <img src="Images/bryant_contracting_bgimage_1.JPG" class="gallery">
+      <img src="Images/bryant_contracting_bgimage_2.JPG" class="gallery">
+      <img src="Images/bryant_contracting_image_1.JPG" class="gallery">
+      <img src="Images/bryant_contracting_bgimage_1.JPG" class="gallery">
+      <img src="Images/bryant_contracting_image_1.JPG" class="gallery">
+  </div>
+  <div class="column">
+      <img src="Images/bryant_contracting_bgimage_2.JPG">
+      <img src="Images/bryant_contracting_image_1.JPG">
+      <img src="Images/bryant_contracting_image_1.JPG">
+      <img src="Images/bryant_contracting_image_1.JPG">
+      <img src="Images/bryant_contracting_bgimage_1.JPG">
+      <img src="Images/bryant_contracting_image_1.JPG">
+  </div>
+  <div class="column">
+      <img src="Images/bryant_contracting_image_1.JPG">
+      <img src="Images/bryant_contracting_image_1.JPG">
+      <img src="Images/bryant_contracting_image_1.JPG">
+      <img src="Images/bryant_contracting_bgimage_1.JPG">
+      <img src="Images/bryant_contracting_image_1.JPG">
+      <img src="Images/bryant_contracting_bgimage_2.JPG">
+      <img src="Images/bryant_contracting_bgimage_2.JPG">
+  </div>
+  <div class="column">
+      <img src="Images/bryant_contracting_bgimage_2.JPG">
+      <img src="Images/bryant_contracting_image_1.JPG">
+      <img src="Images/bryant_contracting_image_1.JPG">
+      <img src="Images/bryant_contracting_image_1.JPG">
+      <img src="Images/bryant_contracting_bgimage_2.JPG">
+      <img src="Images/bryant_contracting_image_1.JPG">
+  </div>
 </div>
     
-<div class="responsive">
-    <div class="gallery">
-        <a target="_blank" href="Images/bryant_contracting_bgimage_1.JPG">
-            <img src="Images/bryant_contracting_bgimage_1.JPG" alt="Image 1" width="600" height="400">
-        </a>
-    </div>
-</div>
+<br>
     
-<div class="responsive">
-    <div class="gallery">
-        <a target="_blank" href="Images/bryant_contracting_bgimage_2.JPG">
-            <img src="Images/bryant_contracting_bgimage_2.JPG" alt="Image 1" width="600" height="400">
-        </a>
-    </div>
-</div>
-    
-<div class="responsive">
-    <div class="gallery">
-        <a target="_blank" href="Images/bryant_contracting_image_1.JPG">
-            <img src="Images/bryant_contracting_image_1.JPG" alt="Image 1" width="600" height="400">
-        </a>
-    </div>
-</div>
-
-    
-<div class="responsive">
-    <div class="gallery">
-        <a target="_blank" href="Images/bryant_contracting_image_1.JPG">
-            <img src="Images/bryant_contracting_image_1.JPG" alt="Image 1" width="600" height="400">
-        </a>
-    </div>
-</div>
-    
-<div class="responsive">
-    <div class="gallery">
-        <a target="_blank" href="Images/bryant_contracting_bgimage_1.JPG">
-            <img src="Images/bryant_contracting_bgimage_1.JPG" alt="Image 1" width="600" height="400">
-        </a>
-    </div>
-</div>
-<!--IDK if I need this-->
-<div class="clearfix"></div>
-
 <?php include 'footer.php';?>
     
 </body>

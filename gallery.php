144d8bb0e9d2a051f1649b61e29c35f4aa209bcc
Jaxon Bryant <jbryant18@piopio.school.nz>
2024-06-17 12:55:05 +1200

    Improved Gallery 1
    Has better responsiveness. It uses "transform" to create the hover effect (instead of the white border I was using)

--- gallery.php
+++ gallery.php
@@ -23,57 +23,55 @@
 <h2>Gallery</h2>
 
 <div class="responsive">
-  <div class="gallery">
-    <a target="_blank" href="Images/bryant_contracting_image_1.JPG">
-      <img src="Images/bryant_contracting_image_1.JPG" alt="Image 1" width="600" height="400">
-    </a>
-    <div class="desc">Image</div>
-  </div>
-</div>
-
-
-<div class="responsive">
-  <div class="gallery">
-    <a target="_blank" href="Images/bryant_contracting_image_1.JPG">
-      <img src="Images/bryant_contracting_image_1.JPG" alt="Image 1" width="600" height="400">
-    </a>
-    <div class="desc">Image</div>
-  </div>
+    <div class="gallery">
+        <a target="_blank" href="Images/bryant_contracting_image_1.JPG">
+            <img src="Images/bryant_contracting_image_1.JPG" alt="Image 1" width="600" height="400">
+        </a>
+    </div>
 </div>
     
 <div class="responsive">
-  <div class="gallery">
-    <a target="_blank" href="Images/bryant_contracting_image_1.JPG">
-      <img src="Images/bryant_contracting_image_1.JPG" alt="Image 1" width="600" height="400">
-    </a>
-    <div class="desc">Image</div>
-  </div>
+    <div class="gallery">
+        <a target="_blank" href="Images/bryant_contracting_bgimage_1.JPG">
+            <img src="Images/bryant_contracting_bgimage_1.JPG" alt="Image 1" width="600" height="400">
+        </a>
+    </div>
 </div>
     
 <div class="responsive">
-  <div class="gallery">
-    <a target="_blank" href="Images/bryant_contracting_image_1.JPG">
-      <img src="Images/bryant_contracting_image_1.JPG" alt="Image 1" width="600" height="400">
-    </a>
-    <div class="desc">Image</div>
-  </div>
+    <div class="gallery">
+        <a target="_blank" href="Images/bryant_contracting_bgimage_2.JPG">
+            <img src="Images/bryant_contracting_bgimage_2.JPG" alt="Image 1" width="600" height="400">
+        </a>
+    </div>
 </div>
     
 <div class="responsive">
-  <div class="gallery">
-    <a target="_blank" href="Images/bryant_contracting_image_1.JPG">
-      <img src="Images/bryant_contracting_image_1.JPG" alt="Image 1" width="600" height="400">
-    </a>
-    <div class="desc">Image</div>
-  </div>
+    <div class="gallery">
+        <a target="_blank" href="Images/bryant_contracting_image_1.JPG">
+            <img src="Images/bryant_contracting_image_1.JPG" alt="Image 1" width="600" height="400">
+        </a>
+    </div>
 </div>
 
+    
+<div class="responsive">
+    <div class="gallery">
+        <a target="_blank" href="Images/bryant_contracting_image_1.JPG">
+            <img src="Images/bryant_contracting_image_1.JPG" alt="Image 1" width="600" height="400">
+        </a>
+    </div>
+</div>
+    
+<div class="responsive">
+    <div class="gallery">
+        <a target="_blank" href="Images/bryant_contracting_bgimage_1.JPG">
+            <img src="Images/bryant_contracting_bgimage_1.JPG" alt="Image 1" width="600" height="400">
+        </a>
+    </div>
+</div>
+<!--IDK if I need this-->
 <div class="clearfix"></div>
-
-<div style="padding:6px;">
-  <p>This example use media queries to re-arrange the images on different screen sizes: for screens larger than 700px wide, it will show four images side by side, for screens smaller than 700px, it will show two images side by side. For screens smaller than 500px, the images will stack vertically (100%).</p>
-  <p>You will learn more about media queries and responsive web design later in our CSS Tutorial.</p>
-</div>
 
 <?php include 'footer.php';?>
     

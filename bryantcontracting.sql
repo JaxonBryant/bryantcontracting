85818c3ad36423dcc0d5569ee5e58907b2e872e1
Jaxon Bryant <jbryant18@piopio.school.nz>
2024-06-24 14:11:17 +1200

    Corrected SQL file
    I accidentally only exported the contacts table, rather than the whole SQL

--- bryantcontracting.sql
+++ bryantcontracting.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 24, 2024 at 03:04 AM
+-- Generation Time: Jun 24, 2024 at 04:09 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 

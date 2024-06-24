6ad54d296b23d40793f4bf4e1f3f02c9076631b7
Jaxon Bryant <jbryant18@piopio.school.nz>
2024-06-24 13:08:14 +1200

    Slightly changed database
    Exported MySQL thing and added to BC folder.
    Added "created_at"
    Its not working, saying there is an error on line 10 of "setup.php"

--- process_form.php
+++ process_form.php
@@ -1,25 +1,22 @@
 <?php
 // Include the database connection setup file
 include 'setup.php';
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    print_r ($_POST);
-    die();
-    /* Escape user inputs for security */
-    $firstname = $conn->real_escape_string($_POST['firstname']);
-    $lastname = $conn->real_escape_string($_POST['lastname']);
+    // Escape user inputs for security
+    $first_name = $conn->real_escape_string($_POST['first_name']);
+    $last_name = $conn->real_escape_string($_POST['last_name']);
     $email = $conn->real_escape_string($_POST['email']);
     $message = $conn->real_escape_string($_POST['message']);
-    /* SQL query to insert form data into the contacts table */
-    $sql = "INSERT INTO contacts (firstname, lastname, email, message, created_at)
-        VALUES ('$firstname', '$lastname', '$email', '$message', NOW())";
-    /* Execute the query and check if it was successful */
+    // SQL query to insert form data into the contacts table
+    $sql = "INSERT INTO contacts (first_name, last_name, email, message, created_at)
+        VALUES ('$first_name', '$last_name', '$email', '$message', NOW())";
+    // Execute the query and check if it was successful
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully";
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        echo "Error: " . $sql . " " . $conn->error;
     }
-/* Close the database connection */
-$conn->close();
+    // Close the database connection
+    $conn->close();
 }
 ?>

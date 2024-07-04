a9b7ca5f052c1c137a43d758167e57c28d41c0a8
Jaxon Bryant <jbryant18@piopio.school.nz>
2024-07-05 10:42:31 +1200

    Working navbar!!!
    I used ChatGPT to help with figuring out the process and format, and it did all the javascript.
    
    It took 2 hours, and the reason it wasn't working was because line 97 and 119 had different numbers (400 and 800) :' )
    
    It probably could be optimised.

--- header.php
+++ header.php
@@ -1,18 +1,58 @@
-<header>
-    <div class="logo">
-        <img src="images/bryant_contracting_logo.jpg" alt="Bryant Contracting" style="width: 90%;">
-    </div>
-    <div class="text" style="background-color: #f1f1f1;">
-        <h1>027 215 4213</h1>
-        <h3>nik@bryantcontracting.co.nz</h3>
-    </div>
-</header>
+<body>
+    <header>
+        <div class="logo">
+            <img src="images/bryant_contracting_logo.jpg" alt="Bryant Contracting" style="width: 90%;">
+        </div>
+        <div class="text" style="background-color: #f1f1f1;">
+            <h1>027 215 4213</h1>
+            <h3>nik@bryantcontracting.co.nz</h3>
+        </div>
+    </header>
+
+    <style>
+
+    </style>
 
 <nav>
     <a href="index.php">Home</a>
-    <a href="agriculture.php">Agriculture</a>
-    <a href="earthworks.php">Earthworks</a>
-    <a href="cartage.php">Cartage</a>
+    <div class="services">
+        <a href="agriculture.php">Agriculture</a>
+        <a href="earthworks.php">Earthworks</a>
+        <a href="cartage.php">Cartage</a>
+    </div>
+    <div class="dropdown">
+        <a href="#" class="dropbtn">Services &#9662;</a>
+        <div class="dropdown-content">
+            <a href="agriculture.php">Agriculture</a>
+            <a href="earthworks.php">Earthworks</a>
+            <a href="cartage.php">Cartage</a>
+        </div>
+    </div>
     <a href="gallery.php">Gallery</a>
-    <a href="contacts.php" class="right">Contact</a>
+    <a href="contacts.php" class="right">Contacts</a>
 </nav>
+
+<script>
+    // JavaScript to handle dropdown behavior
+    document.addEventListener('DOMContentLoaded', function() {
+        var dropdown = document.querySelector('.dropdown');
+        var dropdownContent = document.querySelector('.dropdown-content');
+        var dropbtn = document.querySelector('.dropbtn');
+
+        dropbtn.addEventListener('click', function(event) {
+            event.preventDefault(); // Prevent the default anchor behavior
+            dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+        });
+
+        // Close the dropdown menu if the user clicks outside of it
+        window.onclick = function(event) {
+            if (!event.target.matches('.dropbtn')) {
+                if (dropdownContent.style.display === 'block') {
+                    dropdownContent.style.display = 'none';
+                }
+            }
+        };
+    });
+</script>
+
+</body>

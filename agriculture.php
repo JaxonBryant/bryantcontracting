35c7988470427d0f8eaecd9572db645be676ec5f
Jaxon Bryant <jbryant18@piopio.school.nz>
2024-07-24 10:21:34 +1200

    Updated page names
    Also started working on Agriculture page

--- agriculture.php
+++ agriculture.php
@@ -8,7 +8,7 @@
             <link rel="stylesheet" href="css/style.css"> -->
 
         <!-- page setup -->
-        <title>Bryant Contracting: Home</title>
+        <title>Bryant Contracting: Agriculture</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
             <link rel="stylesheet" href="css/style.css">
@@ -20,7 +20,30 @@
 
     <body>
     <?php include 'header.php';?>
+        <title1>Agriculture</title1>
+        
+        <title2>Silage- Grass</title2>
+        
+            <p>Round bales
+            loader wagons
+            Forage harvesting</p>
 
+        <title2>Silage- Maize</title2>
+        
+            <p>Planting
+            Harvesting </p>
+
+        <title2>Cultivation and planting</title2>
+        
+            <p>Power harrow
+            Rotter tilling
+            Direct drilling - seed- fert- slug bait
+            Roller drill
+            Disc</p>
+
+            <p>Spraying
+            Boom spraying
+            </p>
     <?php include 'footer.php';?>
 
     </body>
--- cartage.php
+++ cartage.php
@@ -8,7 +8,7 @@
             <link rel="stylesheet" href="css/style.css"> -->
 
         <!-- page setup -->
-        <title>Bryant Contracting: Home</title>
+        <title>Bryant Contracting: Cartage</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
             <link rel="stylesheet" href="css/style.css">

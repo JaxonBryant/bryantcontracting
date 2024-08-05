655b5c7dce6dddc3dac76515e9b7842c65d17b01
Jaxon Bryant <jbryant18@piopio.school.nz>
2024-08-06 09:40:21 +1200

    Updated SQL
    It's the same

--- bryantcontracting.sql
+++ bryantcontracting.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 31, 2024 at 12:14 AM
+-- Generation Time: Aug 05, 2024 at 11:39 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
